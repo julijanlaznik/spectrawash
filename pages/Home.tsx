d838f5a4ba8b45c31d9e8a0a21fd05af875a07fd
Julijan Laznik <julijanlaznik@Julijan--MacBook-Air.local>
2025-12-09 11:01:36 +0100

    hide team section

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
 import Portfolio from '../components/Portfolio';
-import Team from '../components/Team';
+// import Team from '../components/Team';
 import Contact from '../components/Contact';
 import SocialFeed from '../components/SocialFeed';
 import Testimonials from '../components/Testimonials';
@@ -23,8 +23,8 @@ const Home: React.FC = () => {
       
       {/* Social Feed serves as a dynamic break before the Team section */}
       <SocialFeed />
-      
-      <Team />
+
+      {/*<Team /> */}
       {/* Partners section removed */}
       <Contact />
       {/* FAQ at the very bottom */}
